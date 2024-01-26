536fc054a10c99192b4495b2e63bca1260c4e2a0
Oona Kärki <oonakarki19@gmail.com>
2024-01-26 10:24:45 +0200

    tutorial step 2

--- myApp.js
+++ myApp.js
@@ -1,4 +1,8 @@
 let express = require("express");
-let app = express(console.log("Hello world"));
+let app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello world");
+});
 
 module.exports = app;
